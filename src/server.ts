a45d8f7d276d3759b6f4eddd7ff3bb2639b91ad6
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-02-26 13:28:25 +0500

    first commit

--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,8 @@ app.use(express.json());
 
 app.use("/", mainRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server ${PORT} portda ishlayapti`);
+  console.log(`Server running on port ${PORT}`);
 });
